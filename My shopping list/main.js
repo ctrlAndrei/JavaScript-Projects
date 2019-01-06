25623195f7ad19de1ab779221a1b8050598490bb
hutustepit <36453564+hutustepit@users.noreply.github.com>
2019-01-06 20:21:54 +0200

    m

--- My shopping list/main.js
+++ My shopping list/main.js
@@ -15,42 +15,39 @@ function addElement(){
     var del = document.createElement('button');
     var edit = document.createElement('button');
     var submit = document.createElement('button');
+    var span = document.createElement('span');
 
     if (input.value===""){
         return;
     }
-    li.innerText = input.value;
+    span.innerText = input.value;
     del.innerText = 'delete';
     del.addEventListener('click',function(){
-        delElement(del);
+        delElement(li);
     })
-    pater.appendChild(li);
-    pater.appendChild(del);
-    pater.appendChild(edit);
     edit.innerText = "edit";
     submit.innerText="submit";
     edit.addEventListener('click',function(){
         submit.hidden=true;
-        li.innerHTML='<input value="'+li.innerText+'">';
-        li.addEventListener('keypress',function(ev){
+        span.innerHTML='<input value="'+span.innerText+'">';
+        span.addEventListener('keypress',function(ev){
             if (ev.key==="Enter"){
-                li.innerHTML=li.children[0].value;
+                span.innerHTML=span.children[0].value;
                 submit.hidden=false;
             }
         })
     })
     submit.addEventListener('click',function(){
-        var add = document.createElement('li');
-        add.innerText=li.innerText;
-        final.appendChild(add);
-        delElement(del);
+        li.innerHTML=li.children[0].innerText;
+        final.appendChild(li);
     })
-    pater.appendChild(submit);
+    li.appendChild(span);
+    li.appendChild(del);
+    li.appendChild(edit);
+    li.appendChild(submit);
+    pater.appendChild(li);
 }
 
 function delElement(d){
-    d.parentNode.removeChild(d.previousElementSibling);
-    d.parentNode.removeChild(d.nextElementSibling);
-    d.parentNode.removeChild(d.nextElementSibling);
     d.parentNode.removeChild(d);
 }
