1298e25fe0fd2e9d909cbff12d9aaf1806cf5fab
andrei <andrei.bucur550@gmail.com>
2019-01-16 17:23:12 +0200

    m

--- Authors/js/app.js
+++ Authors/js/app.js
@@ -1,15 +1,28 @@
-const pater = document.getElementById('pater');
+const first = document.querySelector('#first');
+const second = document.querySelector('#second');
 
-for (let i = 0; i < 6; i++){
+for (let i = 0; i < 10; i++){
+    (i < 5)? generateImage(first) : generateImage(second);
+}
+
+// for (let i = 0 ; i < 5 ; i++){
+//      generateImage(second);
+// }
+
+function generateImage(parent){
     fetch('https://randomuser.me/api/')
     .then( res => res.json())
     .then (res => {
+        let div = document.createElement('div');
         let img = document.createElement('img');
         let label = document.createElement('label');
         label.innerHTML = res.results[0].name.first + ' ' + res.results[0].name.last;
         img.src = res.results[0].picture.large;
-        pater.appendChild(label);
-        pater.appendChild(img);
+        img.style.borderRadius = '15px'
+        div.classList.add('col-sm-2');
+        div.appendChild(img);
+        div.appendChild(label);
+        parent.appendChild(div);
         console.log(res.results[0].picture.large);
     })
 }
