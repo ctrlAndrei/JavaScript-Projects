cfb96af52692d3f7b705ab186a306b00310f31af
mobra-sabi <31726444+mobra-sabi@users.noreply.github.com>
2019-01-27 19:18:28 +0200

    m

--- CatSelect/main.js
+++ CatSelect/main.js
@@ -3,64 +3,82 @@ const buttons = document.getElementById('buttons');
 const images = Array.from(document.querySelectorAll('.images'));
 const select = document.getElementById('select')
 const img = document.getElementById('img');
+const solo = document.getElementById('soloImg');
 
+const configFetch = { headers: { "x-api-key": "8591a1f5-4714-4357-bff7-25f82e06dab8" }};
 
-fetch('https://api.thecatapi.com/v1/images/search',{headers:{"x-api-key":"8591a1f5-4714-4357-bff7-25f82e06dab8"}})
-.then(res => res.json())
-.then(data =>{
-    img.src = data[0].url;
-})
+/**
+ * Afiseaza o poza random
+ */
 
-fetch('https://api.thecatapi.com/v1/breeds',{headers:{"x-api-key":"8591a1f5-4714-4357-bff7-25f82e06dab8"}})
-.then(res => res.json())
-.then(data =>{
-    data.forEach(el=>{
-        let breed = document.createElement('option');
-        breed.value = el.name;
-        breed.innerText = el.name;
-        breed.id = el.id;
-        select.appendChild(breed);
-    })
-})
+fetch('https://api.thecatapi.com/v1/images/search', configFetch)
+    .then(res => res.json())
+    .then(data => {
+        img.src = data[0].url;
+    });
 
-img.addEventListener('click',()=>changeImage());
-select.addEventListener('change',()=>changeImage());
+/**
+ * Populeaza selectul de rase
+ */
 
-function changeImage(){
-    fetch('https://api.thecatapi.com/v1/images/search?breed_id='+ select.options[select.selectedIndex].id,{headers:{"x-api-key":"8591a1f5-4714-4357-bff7-25f82e06dab8"}})
-    .then(res=>res.json())
-    .then(data=>{
-        img.src=data[0].url;
-    })
+fetch('https://api.thecatapi.com/v1/breeds', configFetch)
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(el => select.innerHTML += `<option value="${el.id}">${el.name}</option>`)
+    });
+
+solo.addEventListener('click', ()=> {changeImage(select.value)});
+select.addEventListener('change', ()=> {changeImage(select.value)});
+
+function changeImage(breed = '') {
+    console.log('https://api.thecatapi.com/v1/images/search?breed_id=' + breed);
+    let url = 'https://api.thecatapi.com/v1/images/search?breed_id=' + breed;
+    fetch(url, configFetch)
+        .then(res => res.json())
+        .then(data => {
+            console.dir(data);
+            img.src = data[0].url;
+        })
 }
 
-//category buttons
+//Populeaza category buttons
 
 fetch('https://api.thecatapi.com/v1/categories')
-.then(res=>res.json())
-.then(data=>{
-    data.forEach(el=>{
-        let button = document.createElement('button');
-        button.innerText = el.name;
-        button.id = el.id;
-        button.classList.add('mr-2');
-        button.classList.add('btn');
-        button.classList.add('btn-warning');
-        buttons.appendChild(button);
-        categories.push(button);
-    })
-   
-    console.log(categories)
-    categories.forEach(el=>{
-        el.addEventListener('click',()=>{
-            for(let i = 0; i < 6; i++){
-            fetch('https://api.thecatapi.com/v1/images/search?category_ids=' + el.id,{headers:{"x-api-key":"8591a1f5-4714-4357-bff7-25f82e06dab8"}})
-            .then(res=>res.json())
-            .then(data=>{
-                console.log(data);
-                images[i].src=data[0].url;
-            })
-         } })
-    })
-})
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(el => {
+            let button = document.createElement('button');
+            button.innerText = el.name;
+            button.id = el.id;
+            button.classList.add('mr-2');
+            button.classList.add('btn');
+            button.classList.add('btn-warning');
+            buttons.appendChild(button);
+            button.addEventListener('click', (ev)=>{buttonEvent(ev.target.id)});
+            // categories.push(button);
+        })})
 
+//         console.log(categories);
+//         categories.forEach(el=>{
+        //     el.addEventListener('click',()=>{
+            //     for(let i = 0; i < 6; i++){
+            //     fetch('https://api.thecatapi.com/v1/images/search?category_ids=' + el.id,{headers:{"x-api-key":"8591a1f5-4714-4357-bff7-25f82e06dab8"}})
+            //     .then(res=>res.json())
+            //     .then(data=>{
+            //         console.log(data);
+            //         images[i].src=data[0].url;
+            //     })
+            //  } })
+        // })
+    // })
+
+function buttonEvent(categ){
+    for(let i = 0; i < 6; i++){
+        fetch('https://api.thecatapi.com/v1/images/search?category_ids=' + categ,configFetch)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            images[i].src=data[0].url;
+        })
+     }
+}
