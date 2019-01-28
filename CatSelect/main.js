1560f37295c2f5ad35e7d9a7303ac92bd2fdda30
andrei <andrei.bucur550@gmail.com>
2019-01-29 00:18:04 +0200

    m

--- CatSelect/main.js
+++ CatSelect/main.js
@@ -1,4 +1,5 @@
-let categories = [];
+let breeds = [];
+let input = document.createElement('input');
 const buttons = document.getElementById('buttons');
 const images = Array.from(document.querySelectorAll('.images'));
 const select = document.getElementById('select')
@@ -24,7 +25,7 @@ fetch('https://api.thecatapi.com/v1/images/search', configFetch)
 fetch('https://api.thecatapi.com/v1/breeds', configFetch)
     .then(res => res.json())
     .then(data => {
-        data.forEach(el => select.innerHTML += `<option value="${el.id}">${el.name}</option>`)
+        data.forEach(el => {select.innerHTML += `<option value="${el.id}">${el.name}</option>`; breeds.push(el)})
     });
 
 solo.addEventListener('click', ()=> {changeImage(select.value)});
@@ -55,12 +56,15 @@ fetch('https://api.thecatapi.com/v1/categories')
             button.classList.add('btn-warning');
             buttons.appendChild(button);
             button.addEventListener('click', (ev)=>{buttonEvent(ev.target.id)});
-            // categories.push(button);
         })})
+    .then(()=>{
+        input.placeholder = 'breed search';
+        buttons.appendChild(input);
+    })
 
         gif.addEventListener('click',()=>{
             for(let i = 0; i < 6; i++){
-                fetch('https://api.thecatapi.com/v1/images/search?mime_types=gif',configFetch)
+                fetch('https://api.thecatapi.com/v1/images/search?mime_types=gif', configFetch)
                 .then(res=>res.json())
                 .then(data=>{
                     console.log(data);
@@ -68,23 +72,12 @@ fetch('https://api.thecatapi.com/v1/categories')
                 })
              }
         })
-//         console.log(categories);
-//         categories.forEach(el=>{
-        //     el.addEventListener('click',()=>{
-            //     for(let i = 0; i < 6; i++){
-            //     fetch('https://api.thecatapi.com/v1/images/search?category_ids=' + el.id,{headers:{"x-api-key":"8591a1f5-4714-4357-bff7-25f82e06dab8"}})
-            //     .then(res=>res.json())
-            //     .then(data=>{
-            //         console.log(data);
-            //         images[i].src=data[0].url;
-            //     })
-            //  } })
-        // })
-    // })
+
+
 
 function buttonEvent(categ){
     for(let i = 0; i < 6; i++){
-        fetch('https://api.thecatapi.com/v1/images/search?category_ids=' + categ,configFetch)
+        fetch('https://api.thecatapi.com/v1/images/search?category_ids=' + categ, configFetch)
         .then(res=>res.json())
         .then(data=>{
             console.log(data);
@@ -92,3 +85,46 @@ function buttonEvent(categ){
         })
      }
 }
+
+/**
+ * Cod cat search
+ */
+
+// fetch('https://api.thecatapi.com/v1/breeds')
+// .then(res => res.json())
+// .then(data =>{
+//     data.forEach(el=>{
+//         breeds.push(el);
+//     })
+    
+// })
+
+function showImage(index,id,title){
+    fetch('https://api.thecatapi.com/v1/images/search?breed_id=' + id, configFetch)
+    .then(res=>res.json())
+    .then(data=>{
+        images[index].src = data[0].url;
+        images[index].title = title;
+        images[index].style = "border-radius: 50px; max-block-size: 190px";
+    })
+}
+
+function findBreeds(str){
+    let arr = breeds.filter(el=>{
+        return el.name.toLowerCase().includes(str);
+    })
+    return arr;
+}
+
+input.addEventListener('keyup',()=>{
+    if (input.value.length >= 2){
+        let search = findBreeds(input.value);
+        console.log(search)
+        let length = search.length;
+        if (length > 6){length = 6}
+        for (let i=0,  m = 0; i < length; i++){
+            for(let j = 0; j < 6/length; j++, m++){
+            showImage(m,search[i].id,search[i].name);
+        }}
+    }
+})
